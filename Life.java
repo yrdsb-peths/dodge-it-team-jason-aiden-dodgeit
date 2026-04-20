fcc26b0050d650986dd33c45ea88a19ba9726d2e
348831199 <348831199@gapps.yrdsb.ca>
2026-04-20 12:09:11 -0400

    did stuff

---
+++ Life.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Life here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Life extends Actor
+{
+    
+    public void act()
+    {
+        
+    }
+}
