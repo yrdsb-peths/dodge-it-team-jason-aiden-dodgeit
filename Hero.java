94ef4c6e6e4fdf994997864a7e01cd35bdbb091b
348831199 <348831199@gapps.yrdsb.ca>
2026-04-20 11:36:35 -0400

    hero now moves

--- Hero.java
+++ Hero.java
@@ -2,8 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Hero extends Actor
 {
+    int y = 100;
     public void act()
     {
-        setLocation(100,100);
+        setLocation(100,y);
+        if(Greenfoot.isKeyDown("s"))
+        {
+            y += 3;
+        }
+        if(Greenfoot.isKeyDown("w"))
+        {
+            y -= 3;
+        }
     }
 }
