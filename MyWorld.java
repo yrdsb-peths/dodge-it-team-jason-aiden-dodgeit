039d4fac3aa5ac58f039fabb85c5eb2dfb6cc482
Jason <baifnjdn@gmail.com>
2026-04-20 11:28:41 -0400

    Added banana
    Added banana and the world creates a banana.

--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,8 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 300, 1);
+        super(600, 400, 1);
+        //addObject(new Player(), 100, 150);
+        addObject(new Banana(), 600, Greenfoot.getRandomNumber(400));
     }
 }
