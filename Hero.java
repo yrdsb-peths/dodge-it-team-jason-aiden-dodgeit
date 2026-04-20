9461f627d39d273a91563d0089f34c2da819d315
Jason <baifnjdn@gmail.com>
2026-04-20 12:03:06 -0400

    Fixed images

--- Hero.java
+++ Hero.java
@@ -3,6 +3,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hero extends Actor
 {
     int y = 100;
+
+
+
+    public Hero() {
+        GreenfootImage image = new GreenfootImage("ppl1.png");
+        setImage(image);
+    }
+
+
     public void act()
     {
         setLocation(100,y);
